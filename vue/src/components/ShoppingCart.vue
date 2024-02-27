778c8b025fe6a40631b209fb678296f711691815
amyl164 <amyl81008@gmail.com>
2024-02-26 20:50:28 -0500

    ER

--- vue/src/components/ShoppingCart.vue
+++ vue/src/components/ShoppingCart.vue
@@ -11,7 +11,7 @@
             <button @click="remove()" class="btn">Remove</button>
         </div>
     </div>
-    <h2 class="cost">Total Cost:</h2>
+    <h2 class="cost">Total Cost:${{ total()}}</h2>
     </div>
 </template>
 
@@ -28,9 +28,20 @@ function remove(){
             button.addEventListener("click",function(event){
                 const objectToRemove =  event.target.parentElement;
                 objectToRemove.remove();
+                const remove = store.cart
+                remove.splice(0,1);
             })})};
 
-
+function total(){
+    const arr = store.cart
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i].price
+    } 
+    
+    console.log(sum)
+    return sum
+}      
 </script>
 
 <style scoped>
@@ -40,7 +51,6 @@ width: 30%;
 position:absolute;
 top:0; right:0; 
 height: 889vh;
-
 }
 .title2{
 text-align:center;
@@ -52,6 +62,12 @@ img {
 }
 .cartt,h2{
     text-align:center;
+    align-items: center;
 }
-
+.btn{
+    background-color: red;
+    border-radius: 0.3rem;
+    font-size: 1rem;
+    
+}
 </style>
